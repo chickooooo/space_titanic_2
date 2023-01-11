0471216e796131590216451554b04527fb063bd2
Kshitij Rajale <kshitijrajale@gmail.com>
2023-01-11 14:31:17 +0530

    addded pipeline documentation

--- pipelines/data_preparation.py
+++ pipelines/data_preparation.py
@@ -3,22 +3,39 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class CustomDataPreparation(BaseEstimator, TransformerMixin):
+    """Update column names, fill missing values, create new features
+    """
 
+    # column names as per naming conventions
     column_names = ["passenger_id", "home_planet", "cryo_sleep", "cabin", "destination", "age", "vip", "room_service", "food_court", "shopping_mall", "spa", "vr_deck", "name"]
     
     def __init__(self) -> None:
+        # will hold feature aggregates
         self.aggregates = {}
 
     def fit(self, X: pd.DataFrame):
+        # get feature aggregates
         self.__get_aggregates(X)
         return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """transforms training or test data into required dataset
+
+        Args:
+            X (pd.DataFrame): training or test data
+
+        Returns:
+            pd.DataFrame: required dataset
+        """
+
+        # required data
         data = pd.DataFrame()
+
         x_copy = X.copy()
+        # updating column names
         x_copy.columns = self.column_names
 
-        # filling null values of current column
+        # filling null values with aggregates
         for column in self.column_names:
             if column not in ["passenger_id", "cabin", "name"]:
                 data[column] = x_copy[column].fillna(value=self.aggregates[column])
@@ -30,21 +47,33 @@ class CustomDataPreparation(BaseEstimator, TransformerMixin):
         data["deck"] = x_copy.cabin.apply(lambda x: x.split("/")[0] if pd.notna(x) else x)
         data["num_in_cabin"] = x_copy.cabin.apply(lambda x: x.split("/")[1] if pd.notna(x) else x)
         data["side"] = x_copy.cabin.apply(lambda x: x.split("/")[2] if pd.notna(x) else x)
+        # filling null values
         data.deck.fillna(value=self.aggregates["deck"], inplace=True)
         data.side.fillna(value=self.aggregates["side"], inplace=True)
         data.num_in_cabin.fillna(value=self.aggregates["num_in_cabin"], inplace=True)
+        # changing type from object to int
         data.num_in_cabin = data.num_in_cabin.astype("int")
 
+        # combining spending
         data["total_spending"] = data.room_service + data.food_court + data.shopping_mall + data.spa + data.vr_deck
 
+        # creating age category
         data["age_category"] = data.age.apply(lambda x: self.__get_age_category(x))
 
+        # creating gender feature
         data["gender"] = x_copy.name.apply(lambda x: self.__get_gender(x))
+        # filling missing gender with male
         data.gender.fillna(value=1.0, inplace=True)
 
         return data
 
     def __get_aggregates(self, X: pd.DataFrame) -> None:
+        """saves aggregate value for each feature
+
+        Args:
+            X (pd.DataFrame): training dataset
+        """
+
         self.aggregates["home_planet"] = X.HomePlanet.mode()[0]
         self.aggregates["cryo_sleep"] = X.CryoSleep.mode()[0]
 
@@ -58,6 +87,7 @@ class CustomDataPreparation(BaseEstimator, TransformerMixin):
         self.aggregates["vip"] = X.VIP.mode()[0]
         self.aggregates["age"] = X.Age.median()
 
+        # default value of 0.0
         self.aggregates["room_service"] = 0.0
         self.aggregates["food_court"] = 0.0
         self.aggregates["shopping_mall"] = 0.0
@@ -65,6 +95,14 @@ class CustomDataPreparation(BaseEstimator, TransformerMixin):
         self.aggregates["vr_deck"] = 0.0
 
     def __get_age_category(self, age: float) -> str:
+        """create age category from age feature
+
+        Args:
+            age (float): age of passenger
+
+        Returns:
+            str: age category to which the passenger belongs
+        """
         if age <= 16:
             return "child"
         elif age <= 30:
@@ -75,6 +113,14 @@ class CustomDataPreparation(BaseEstimator, TransformerMixin):
             return "old_adult"
 
     def __get_gender(self, name: str) -> int:
+        """identify gender from name feature
+
+        Args:
+            name (str): name of passenger
+
+        Returns:
+            int: 1 -> male, 0 -> female
+        """
         if pd.isna(name):
             return np.nan
         if name.split(" ")[0][-1] in ["a", "e", "i", "y"]:
--- pipelines/data_preprocessing.py
+++ pipelines/data_preprocessing.py
@@ -3,10 +3,16 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import RobustScaler
 
 class CustomDataPreprocessing(BaseEstimator, TransformerMixin):
+    """Encodes categorical features, transforms boolean features and scales Numerical features
+    """
 
+    # categorical features
     categorical_attributes = ["home_planet", "destination", "deck", "side", "age_category"]
+    # boolean features
     boolean_attributes = ["cryo_sleep", "vip"]
+    # numerical features
     num_attributes = ["age", "room_service", "food_court", "shopping_mall", "spa", "vr_deck", "group", "number_in_group", "num_in_cabin", "total_spending"]
+    # column order of final data
     column_order = ['age', 'room_service', 'food_court', 'shopping_mall', 'spa', 'vr_deck',
        'group', 'number_in_group', 'num_in_cabin', 'total_spending',
        'home_planet_Earth', 'home_planet_Europa', 'home_planet_Mars',
@@ -18,31 +24,39 @@ class CustomDataPreprocessing(BaseEstimator, TransformerMixin):
        'vip', 'gender']
 
     def __init__(self) -> None:
+        # creates scaler object
         self.scaler = RobustScaler()
 
     def fit(self, X: pd.DataFrame):
+        # fits numerical data to scaler ovject
         self.scaler.fit(X[self.num_attributes])
         return self
     
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        # seperating categorical data
         cat_data = X[self.categorical_attributes].copy()
+        # seperating boolean data
         bool_data = X[self.boolean_attributes].copy()
+        # seperating numerical data
         num_data = X[self.num_attributes].copy()
         num_index = num_data.index
 
-        # cat data
+        # encoding cat data
         cat_data_encoded = pd.get_dummies(cat_data)
 
-        # bool data
+        # transforming bool data
         bool_data_encoded = pd.DataFrame()
         for attribute in self.boolean_attributes:
             bool_data_encoded[attribute] = bool_data[attribute].apply(lambda x: 1 if x else 0)
 
-        # num data
+        # scaling num data
         num_data_scaled = self.scaler.transform(num_data)
+        # converting NDArray to DataFrame
         num_data_scaled = pd.DataFrame(num_data_scaled, columns=self.num_attributes, index=num_index)
 
+        # combining into single dataframe
         data: pd.DataFrame = pd.concat([num_data_scaled, cat_data_encoded, bool_data_encoded, X.gender], axis=1)
+        # preserving order of columns
         data = data[self.column_order]
 
         return data
